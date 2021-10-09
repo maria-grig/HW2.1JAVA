62c7ba8148ef2507fd5ee452a75f1818e497a022
maria-grig <masha.grigorenko.89@gmail.com>
2021-10-09 23:26:15 +0300

    origin

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5_350;
+        int bonusStake = 20;
+        int bonus = ticketPrice / bonusStake;
+
+        System.out.println("Вы заработали " + bonus + " бонусных миль");
+            }
+
+    }
